708335909b364b8ccf9eec6d92bdc8c4d8f5faba
edgar <tomasov@intaro.mail>
2021-08-13 10:06:13 +0300

    test 1 555

--- app/src/main/java/com/et/testimageskotlin/ui/dialogs/ImageDialog.kt
+++ app/src/main/java/com/et/testimageskotlin/ui/dialogs/ImageDialog.kt
@@ -15,7 +15,8 @@ import com.et.testimageskotlin.models.ImgModel
 class ImageDialog(context: Context,
                   private val imgModel: ImgModel) : Dialog(context) {
 
-    // TEST TEST TEST
+    // TEST 1
+
     private lateinit var main_cl: ConstraintLayout
     private lateinit var title: TextView
     private lateinit var dialog_img: ImageView
